18ff5a310a997f19cac3eacee7b856f19ee62025
BitPaw - the path weaver <42204934+BitPaw@users.noreply.github.com>
2024-07-24 09:44:44 +0200

    Create PXVideo.c

---
+++ Code/PXUltimaC/OS/Hardware/PXVideo.c
@@ -0,0 +1,18 @@
+
+#include <windows.h>
+#include <mfapi.h> // Windows Vista
+#pragma comment(lib, "Mfplat.lib")
+
+void PXAPI PXVideoCaptureDeviceList()
+{
+  
+  
+}
+
+
+
+
+
+
+# https://learn.microsoft.com/en-us/windows/win32/medfound/enumerating-video-capture-devices
+# https://learn.microsoft.com/en-us/windows/win32/medfound/audio-video-capture-in-media-foundation
