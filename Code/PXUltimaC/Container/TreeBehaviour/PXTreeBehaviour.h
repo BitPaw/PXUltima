a37cc2f6f8a2adbb6eda32d55276c6c7caf8c72f
BitPaw - the path weaver <42204934+BitPaw@users.noreply.github.com>
2024-11-07 17:01:00 +0100

    Update PXTreeBehaviour.h

--- Code/PXUltimaC/Container/TreeBehaviour/PXTreeBehaviour.h
+++ Code/PXUltimaC/Container/TreeBehaviour/PXTreeBehaviour.h
@@ -1,6 +1,22 @@
 #ifndef PXTreeBehaviourDEFINED
 #define PXTreeBehaviourDEFINED
 
+typedef (PXAPI* PXTreeBehaviourNodeCallBack)(void* owner);
+
+#define PXTreeBehaviourNodeFlagMask
+#define PXTreeBehaviourNodeFlagActive // If not, node will be ignored and skipped
+#define PXTreeBehaviourNodeFlagCanAbord // Can an action me canceled before or while it is running? Will not consume object
+#define PXTreeBehaviourNodeFlagImportant // Node shall be executed if possible. Useful when we want to swap threads or aboud, so we still execute this node 
+
+
+typedef struct PXTreeBehaviourNode_
+{
+    
+    PXTreeBehaviourNodeCallBack CallBack;
+}
+PXTreeBehaviourNode;
+
+
 typedef struct PXTreeBehaviour_
 {
     int dummy;
