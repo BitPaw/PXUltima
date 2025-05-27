70c578cfe1315d45405eddb73a806fe214cbbe8a
BitPaw - the path weaver <NozsBitPaw@gmail.com>
2025-05-27 23:57:08 +0200

    Update PXPhysic.h

--- Code/PXUltimaC/PX/Math/PXPhysic.h
+++ Code/PXUltimaC/PX/Math/PXPhysic.h
@@ -5,17 +5,17 @@
 #include <PX/Math/PXVector.h>
 #include <PX/Math/PXMatrix.h>
 
-#define PXGravityForceSun 274
-#define PXGravityForceMercury 3.7
-#define PXGravityForceVenus 8.87
-#define PXGravityForceEarth 9.807
+#define PXGravityForceSun       274
+#define PXGravityForceMercury   3.7
+#define PXGravityForceVenus     8.87
+#define PXGravityForceEarth     9.807
 #define PXGravityForceEarthMoon 1.62
-#define PXGravityForceMars 3.721
-#define PXGravityForceJupiter 24.79
-#define PXGravityForceSaturn 10.44
-#define PXGravityForceNeptune 11.15
-#define PXGravityForceUranus 8.87
-#define PXGravityForcePluto 0.62
+#define PXGravityForceMars      3.721
+#define PXGravityForceJupiter   24.79
+#define PXGravityForceSaturn    10.44
+#define PXGravityForceNeptune   11.15
+#define PXGravityForceUranus    8.87
+#define PXGravityForcePluto     0.62
 
 typedef struct PXPhysicProperties_
 {
