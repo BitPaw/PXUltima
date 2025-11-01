aa91f061a630b9e1b041e32e3a28d41b61daa002
BitPaw - the path weaver <NozsBitPaw@gmail.com>
2025-11-01 23:59:14 +0100

    Update PXDictionary.h

--- Code/PXUltimaC/PX/Container/Dictionary/PXDictionary.h
+++ Code/PXUltimaC/PX/Container/Dictionary/PXDictionary.h
@@ -31,27 +31,27 @@ typedef struct PXDictionaryEntry_
 }
 PXDictionaryEntry;
 
-PXPublic void PXAPI PXDictionaryConstruct(PXDictionary* const dictionary, const PXSize keySize, const PXSize valueSize, const PXDictionaryValueLocality pxDictionaryValueLocality);
-PXPublic void PXAPI PXDictionaryDestruct(PXDictionary* const dictionary);
+PXPublic void PXAPI PXDictionaryConstruct(PXDictionary PXREF dictionary, const PXSize keySize, const PXSize valueSize, const PXDictionaryValueLocality pxDictionaryValueLocality);
+PXPublic void PXAPI PXDictionaryDestruct(PXDictionary PXREF dictionary);
 
-PXPublic PXSize PXAPI PXDictionaryValueSize(const PXDictionary* const dictionary);
+PXPublic PXSize PXAPI PXDictionaryValueSize(const PXDictionary PXREF dictionary);
 
-PXPublic void PXAPI PXDictionaryResize(PXDictionary* const dictionary, const PXSize entrys);
+PXPublic void PXAPI PXDictionaryResize(PXDictionary PXREF dictionary, const PXSize entrys);
 
 //-------------------------------------
 // Entry
-PXPublic PXResult PXAPI PXDictionaryEntryCreate(PXDictionary* const dictionary, const void* key, const void** value);
-PXPublic PXResult PXAPI PXDictionaryEntryAdd(PXDictionary* const dictionary, const void* key, const void* value);
-PXPublic PXResult PXAPI PXDictionaryEntryFind(PXDictionary* const dictionary, const void* const key, void** const valueResult);
+PXPublic PXResult PXAPI PXDictionaryEntryCreate(PXDictionary PXREF pxDictionary, const void* key, void** value);
+PXPublic PXResult PXAPI PXDictionaryEntryAdd(PXDictionary PXREF pxDictionary, const void* key, const void* value);
+PXPublic PXResult PXAPI PXDictionaryEntryFind(PXDictionary PXREF pxDictionary, const void PXREF key, void* PXREF valueResult);
 
-PXPublic PXBool PXAPI PXDictionaryEntryAddMultible(PXDictionary* const dictionary, const void** key, const void** value, const PXSize amount);
-PXPublic void PXAPI PXDictionaryRemove(PXDictionary* const dictionary, const void* key);
-PXPublic PXBool PXAPI PXDictionaryRemoveFound(PXDictionary* const dictionary, const void* key, void* const value);
+PXPublic PXBool PXAPI PXDictionaryEntryAddMultible(PXDictionary PXREF dictionary, const void** key, const void** value, const PXSize amount);
+PXPublic void PXAPI PXDictionaryRemove(PXDictionary PXREF dictionary, const void* key);
+PXPublic PXBool PXAPI PXDictionaryRemoveFound(PXDictionary PXREF dictionary, const void* key, void PXREF value);
 //-------------------------------------
 
 // Removed the object and returns it
-PXPublic PXBool PXAPI PXDictionaryExtract(PXDictionary* const dictionary, const void* const key, void* const value);
-PXPublic void PXAPI PXDictionaryIndex(const PXDictionary* const dictionary, const PXSize index, PXDictionaryEntry* const pxDictionaryEntry);
+PXPublic PXBool PXAPI PXDictionaryExtract(PXDictionary PXREF dictionary, const void PXREF key, void PXREF value);
+PXPublic void PXAPI PXDictionaryIndex(const PXDictionary PXREF dictionary, const PXSize index, PXDictionaryEntry PXREF pxDictionaryEntry);
 
 
 #endif
