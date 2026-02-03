39e7f1f8b564d0590cacbc69f3776858c063caa5
BitPaw - the path weaver <NozsBitPaw@gmail.com>
2026-02-03 01:40:50 +0100

    Imageset allignment updated

--- Code/PXUltimaC/PX/Media/D8X/PXD8X.c
+++ Code/PXUltimaC/PX/Media/D8X/PXD8X.c
@@ -1,10 +1,43 @@
 #include "PXD8X.h"
 
 #include <PX/OS/PXOS.h>
-#include <PX/Media/DEFLATE/PXDEFLATE.h>
+#include <PX/Media/ZLIB/PXZLIB.h>
 #include <PX/OS/Console/PXConsole.h>
+#include <compressapi.h>
+#include <PX/Media/DDS/PXDDS.h>
+#include <ddraw.h>
 
 const char PXD8XName[] = "D8X";
+const char PXD8WName[] = "D8W";
+
+
+
+
+const PXI32U PXDDSHeaderHeaderBindingAA[] =
+{
+PXTypeI32ULE,//  Size
+PXTypeI32ULE,//  Flags
+PXTypeI32ULE,//  Height
+PXTypeI32ULE,//  Width
+PXTypeI32ULE,//  PitchOrLinearSize
+PXTypeI32ULE,//  Depth
+PXTypeI32ULE,//  MipMapCount
+PXTypeText(44),// Name
+PXTypeI32ULE,//  PixelFormatSize
+PXTypeI32ULE,//  PixelFormatFlags
+PXTypeText(4),// PixelFormatID[4]
+PXTypeI32ULE,// RGBBitCount
+PXTypeI32ULE,// RBitMask
+PXTypeI32ULE,// GBitMask
+PXTypeI32ULE,// BBitMask
+PXTypeI32ULE,// ABitMask
+PXTypeI32ULE,// Caps1
+PXTypeI32ULE,// Caps2
+PXTypeI32ULE,// Caps3
+PXTypeI32ULE,// Caps4
+PXTypeI32ULE // Reserved2
+};
+const PXI8U PXDDSHeaderHeaderBindingSizeAA = sizeof(PXDDSHeaderHeaderBindingAA) / sizeof(PXI32U);
 
 PXResult PXAPI PXD8XLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo)
 {
@@ -47,13 +80,13 @@ PXResult PXAPI PXD8DatLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo)
         return PXActionRefusedIndexOutOfBounce;
     }
 
-    pxD8XDat->PXD8XDatEntry = PXMemoryHeapCallocT(PXD8XDatEntry, pxD8XDat->TableEntryAmount);
+    pxD8XDat->PXD8XDatEntry = PXMemoryHeapCallocT(PXD8XArchiveEntry, pxD8XDat->TableEntryAmount);
 
     PXFileCursorMoveTo(pxFile, pxD8XDat->TableOffset);
 
     for(PXI32U i = 0; i < pxD8XDat->TableEntryAmount; ++i)
     {
-        PXD8XDatEntry* pxD8XDatEntry = &pxD8XDat->PXD8XDatEntry[i];
+        PXD8XArchiveEntry* pxD8XDatEntry = &pxD8XDat->PXD8XDatEntry[i];
 
         PXFileReadI32U(pxFile, &pxD8XDatEntry->Hash);
         PXFileReadI32U(pxFile, &pxD8XDatEntry->Offset);
@@ -85,7 +118,7 @@ PXResult PXAPI PXD8DatLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo)
 
     for(PXI32U i = 0; i < pxD8XDat->TableEntryAmount; ++i)
     {
-        PXD8XDatEntry* pxD8XDatEntry = &pxD8XDat->PXD8XDatEntry[i];
+        PXD8XArchiveEntry* pxD8XDatEntry = &pxD8XDat->PXD8XDatEntry[i];
 
         // Jump to data
         PXFileCursorMoveTo(pxFile, pxD8XDatEntry->Offset);
@@ -94,15 +127,13 @@ PXResult PXAPI PXD8DatLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo)
         PXBool isCompressed = pxD8XDatEntry->CompressedSize != 0;
 
         
-        pxD8XDatEntry->FileData = PXFileCreate();
-
-        PXFileOpenInfo pxFileOpenInfo;
-        PXClear(PXFileOpenInfo, &pxFileOpenInfo);
+        PXFileCreateInfo pxFileOpenInfo;
+        PXClear(PXFileCreateInfo, &pxFileOpenInfo);
         pxFileOpenInfo.AccessMode = PXAccessModeWriteOnly;
         pxFileOpenInfo.FileSizeRequest = pxD8XDatEntry->DecompressedSize+4096*10;
         pxFileOpenInfo.FlagList = PXFileIOInfoFileVirtual;
 
-        PXFileOpen(pxD8XDatEntry->FileData, &pxFileOpenInfo);
+        PXFileCreate(&pxD8XDatEntry->FileData, &pxFileOpenInfo);
            
 
         if(isCompressed)
@@ -110,8 +141,8 @@ PXResult PXAPI PXD8DatLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo)
             // We dont have one single continius ZLIB stream, it is chunked.. for some reason.
             PXI32U offset = 0;
             PXI32S compressedSize = 0;
-            PXFile* pxFileChunk = PXFileCreate();
-            PXFile* pxFileChunkOut = PXFileCreate();
+            PXFile* pxFileChunk = PXNull;
+            PXFile* pxFileChunkOut = PXNull;
 
             for(PXI32U chunkIndex = 0; chunkIndex < pxD8XDatEntry->ChunksCount; ++chunkIndex)
             {
@@ -124,20 +155,20 @@ PXResult PXAPI PXD8DatLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo)
                 PXFileCursorMoveTo(pxFile, offset);
 
                 {
-                    PXFileOpenInfo pxFileOpenInfo;
-                    PXClear(PXFileOpenInfo, &pxFileOpenInfo);
+                    PXFileCreateInfo pxFileOpenInfo;
+                    PXClear(PXFileCreateInfo, &pxFileOpenInfo);
                     pxFileOpenInfo.AccessMode = PXAccessModeReadAndWrite;
                     pxFileOpenInfo.MemoryCachingMode = PXMemoryCachingModeSequential;
                     pxFileOpenInfo.FlagList = PXFileIOInfoFileMemory;
                     PXBufferSet
                     (
                         &pxFileOpenInfo.Data,
-                        (PXSize)PXFileDataAtCursor(pxFile) + 2,
-                        compressedSize - 2
+                        (PXSize)PXFileDataAtCursor(pxFile), // +2??
+                        compressedSize// - 2
                     );
-                    PXFileOpen(pxFileChunk, &pxFileOpenInfo);
+                    PXFileCreate(&pxFileChunk, &pxFileOpenInfo);
 
-                    PXClear(PXFileOpenInfo, &pxFileOpenInfo);
+                    PXClear(PXFileCreateInfo, &pxFileOpenInfo);
                     pxFileOpenInfo.AccessMode = PXAccessModeReadAndWrite;
                     pxFileOpenInfo.MemoryCachingMode = PXMemoryCachingModeSequential;
                     pxFileOpenInfo.FlagList = PXFileIOInfoFileMemory;
@@ -147,12 +178,12 @@ PXResult PXAPI PXD8DatLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo)
                         PXFileDataAtCursor(pxD8XDatEntry->FileData),
                         PXFileRemainingSize(pxD8XDatEntry->FileData)
                     );
-                    PXFileOpen(pxFileChunkOut, &pxFileOpenInfo);
+                    PXFileCreate(&pxFileChunkOut, &pxFileOpenInfo);
                 }
 
                 //PXFileCursorMoveTo(pxFileChunk, 2);                
 
-                PXDEFLATEParse(pxFileChunk, pxFileChunkOut);
+                PXZLIBDecompress(pxFileChunk, pxFileChunkOut);
 
                 PXFileCursorAdvance(pxD8XDatEntry->FileData, PXFileDataPosition(pxFileChunkOut));
 
@@ -167,6 +198,42 @@ PXResult PXAPI PXD8DatLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo)
             PXFileReadB(pxFile, pxD8XDatEntry->FileData, pxD8XDatEntry->DecompressedSize);
         }
 
+        PXBuffer* pxBuffer = PXFileBufferGET(pxD8XDatEntry->FileData);
+        PXBool isD8W = PXTextCompareA
+        (
+            &pxBuffer->Data[28],
+            3,
+            "DXT",
+            3,
+            PXTextCompareRequireSameLength
+        );
+
+        if(isD8W)
+        {
+            isD8W = PXTextCompareA
+            (
+                &pxBuffer->Data[76],
+                3,
+                "DXT",
+                3,
+                PXTextCompareRequireSameLength
+            );
+        }
+    
+        if(isD8W)
+        {
+            PXSize oldPos = PXFileDataPosition(pxD8XDatEntry->FileData);
+
+            PXFileCursorToBeginning(pxD8XDatEntry->FileData);
+
+            PXECSCreateInfo pxECSCreateInfoSub;
+            PXClear(PXECSCreateInfo, &pxECSCreateInfoSub);
+            pxECSCreateInfoSub.FileCurrent = pxD8XDatEntry->FileData;
+
+            PXD8WLoadFromFile(&pxECSCreateInfoSub);
+
+            PXFileCursorMoveTo(pxD8XDatEntry->FileData, oldPos);
+        }
 
 
         char fileName[64];
@@ -180,13 +247,280 @@ PXResult PXAPI PXD8DatLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo)
     return PXResultOK;
 }
 
+void PXAPI PXD8GGeometryVertexBlock(PXD8GeometryVertexBlock PXREF pxD8GeometryVertexBlock, PXFile PXREF pxFile)
+{
+    PXD8GGeometryVertexFVFParse(&pxD8GeometryVertexBlock->vertices, pxFile);
+    PXFileReadI32U(pxFile, &pxD8GeometryVertexBlock->definition_count);
+
+    for(PXI32U i = 0; i < pxD8GeometryVertexBlock->definition_count; ++i)
+    {
+        PXD8GeometryVertexFVF* pxD8GeometryVertexFVFsSub = &pxD8GeometryVertexBlock->definitions[i];
+
+        PXD8GGeometryVertexFVFParse(pxD8GeometryVertexFVFsSub, pxFile);
+    }
+
+    PXFileReadI32U(pxFile, &pxD8GeometryVertexBlock->BytesBetweenVertices);
+    PXFileReadI32U(pxFile, &pxD8GeometryVertexBlock->NumberOfVertices);
+}
+
+void PXAPI PXD8GGeometryVertexFVFParse(PXD8GeometryVertexFVF PXREF pxD8GeometryVertexFVF, PXFile PXREF pxFile)
+{
+    PXFileReadI16U(pxFile, &pxD8GeometryVertexFVF->Flag);
+    PXFileReadI16U(pxFile, &pxD8GeometryVertexFVF->Offset);
+    PXFileReadI16U(pxFile, &pxD8GeometryVertexFVF->Format);
+    PXFileReadI8U(pxFile, &pxD8GeometryVertexFVF->VertexType);
+    PXFileReadI8U(pxFile, &pxD8GeometryVertexFVF->UVChannelIndex);
+}
+
 PXResult PXAPI PXD8GLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo)
 {
+    PXFile* pxFile = pxResourceLoadInfo->FileCurrent;
+
+    PXD8Geometry dummy;
+    PXClear(PXD8XDat, &dummy);
+
+    PXD8Geometry* pxD8Geometry = &dummy;
+
+    PXFileReadI32U(pxFile, &pxD8Geometry->Header.VertexType);
+    PXFileReadI32U(pxFile, &pxD8Geometry->Header.FaceType);
+    PXFileReadI32U(pxFile, &pxD8Geometry->Header.MeshAmount);
+
+    pxD8Geometry->MeshList = PXMemoryHeapCallocT(PXD8GeometryMesh, pxD8Geometry->Header.MeshAmount);
+
+    for(PXSize i = 0; i < pxD8Geometry->Header.MeshAmount; ++i)
+    {
+        PXD8GeometryMesh* mesh = &pxD8Geometry->MeshList[i];
+
+        PXFileReadI32U(pxFile, &mesh->TextureID);
+        PXFileReadB(pxFile, &mesh->Position, sizeof(float) * 4);
+        PXFileReadI32U(pxFile, &mesh->RadiusBoundingSphere);
+        PXFileReadI32U(pxFile, &mesh->DistanceLODMin);
+        PXFileReadI32U(pxFile, &mesh->DistanceLODMax);
+        PXFileReadI32U(pxFile, &mesh->UnknownA);
+
+        if(0 == mesh->UnknownA)
+        {
+            PXFileReadI32U(pxFile, &mesh->UnknownB);
+            PXFileReadI32U(pxFile, &mesh->UnknownC);
+            PXFileReadI32U(pxFile, &mesh->UnknownD);
+        }
+        else
+        {
+            PXFileCursorRewind(pxFile, sizeof(PXI32U));
+        }
+
+        PXD8GGeometryVertexBlock(&mesh->VertexBlock, pxFile);
+
+        PXFileReadI32U(pxFile, &mesh->face_count);
+        PXFileReadB(pxFile, mesh->IndexList, sizeof(PXI16U) * mesh->face_count);
+        PXFileReadI32U(pxFile, &mesh->node_count);
+        PXFileReadB(pxFile, mesh->nodes, sizeof(PXMatrix4x4F) * mesh->node_count);
+    }
+
     return PXResultOK;
 }
 
 PXResult PXAPI PXD8WLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo)
 {
+    PXFile* pxFile = pxResourceLoadInfo->FileCurrent;
+
+
+    PXD8TextureTable dummy;
+    PXClear(PXD8TextureTable, &dummy);
+
+    PXD8TextureTable* textureTable = &dummy;
+
+    PXFileReadI32U(pxFile, &textureTable->TextureAmount);
+    PXFileReadI32U(pxFile, &textureTable->SetAmount);
+    PXFileReadI32U(pxFile, &textureTable->TotalSizeRequested);
+
+    textureTable->SetList = PXMemoryHeapCallocT(PXD8TextureSet, textureTable->SetAmount);
+    textureTable->EntryList = PXMemoryHeapCallocT(PXD8TextureTableEntry, textureTable->TextureAmount);
+
+    PXI32U totalIndex = 0;
+
+    // For every set..
+    for(PXI32U setIndex = 0; setIndex < textureTable->SetAmount; ++setIndex)
+    {
+        PXD8TextureSet* pxD8TextureSet = &textureTable->SetList[setIndex];
+
+        char* point = PXFileDataAtCursor(pxFile);
+
+        PXFileReadI32U(pxFile, &pxD8TextureSet->TextureSetDataSize);
+        PXFileReadI32U(pxFile, &pxD8TextureSet->TextureTotalSize);
+        PXFileReadI32U(pxFile, &pxD8TextureSet->TextureEntryAmount);
+
+#if PXLogEnable
+        PXLogPrint
+        (
+            PXLoggingInfo,
+            PXD8WName,
+            "Texture",
+            "Set (%i/%i), Amount:<%i>",
+            setIndex +1,
+            textureTable->SetAmount,
+            pxD8TextureSet->TextureEntryAmount
+        );
+#endif
+
+        // In each set, list our list
+        for(PXI32U textureIndex = 0; textureIndex < pxD8TextureSet->TextureEntryAmount; ++textureIndex)
+        {            
+            PXD8TextureTableEntry* pxD8TextureTableEntry = &textureTable->EntryList[totalIndex];
+
+            ++totalIndex;
+
+            char* point = PXFileDataAtCursor(pxFile);
+
+            PXFileReadI32U(pxFile, &pxD8TextureTableEntry->RawDataSize);
+            PXFileReadB(pxFile, pxD8TextureTableEntry->PixelFormat, 4);
+            PXFileReadI32U(pxFile, &pxD8TextureTableEntry->TextureWidth);
+            PXFileReadI32U(pxFile, &pxD8TextureTableEntry->TextureHeight);
+            PXFileReadI32U(pxFile, &pxD8TextureTableEntry->MinmapCount);
+            PXFileReadI32U(pxFile, &pxD8TextureTableEntry->UnknownB);
+            PXFileReadI32U(pxFile, &pxD8TextureTableEntry->UnknownC);
+            PXFileReadI32U(pxFile, &pxD8TextureTableEntry->UnknownD);
+            PXFileReadI32U(pxFile, &pxD8TextureTableEntry->UnknownE);
+            PXFileReadI32U(pxFile, &pxD8TextureTableEntry->UnknownF);
+            PXFileReadI32U(pxFile, &pxD8TextureTableEntry->UnknownG);
+            PXFileReadI32U(pxFile, &pxD8TextureTableEntry->UnknownH);
+
+
+#if PXLogEnable
+            PXLogPrint
+            (
+                PXLoggingInfo,
+                PXD8WName,
+                "Texture",
+                "%c%c%c%c %4ix%-4i (%8i)",
+                pxD8TextureTableEntry->PixelFormat[0],
+                pxD8TextureTableEntry->PixelFormat[1],
+                pxD8TextureTableEntry->PixelFormat[2],
+                pxD8TextureTableEntry->PixelFormat[3],
+                pxD8TextureTableEntry->TextureWidth,
+                pxD8TextureTableEntry->TextureHeight,
+                pxD8TextureTableEntry->RawDataSize
+            );
+#endif
+
+        }
+    }
+
+
+
+
+
+    
+#if 0
+    DECOMPRESSOR_HANDLE h;
+    CreateDecompressor(COMPRESS_ALGORITHM_DEFLATE, NULL, &h);
+
+    Decompress(h, input, inputSize, output, outputSize, &bytesDecompressed);
+
+    CloseDecompressor(h);
+#endif
+
+
+
+    for(PXI32U i = 0; i < textureTable->TextureAmount; ++i)
+    {
+        PXD8TextureTableEntry* pxD8TextureTableEntry = &textureTable->EntryList[i];
+
+
+
+        PXFileCreateInfo pxFileCreateInfo;
+        PXClear(PXFileCreateInfo, &pxFileCreateInfo);
+        pxFileCreateInfo.AccessMode = PXAccessModeReadAndWrite;
+        pxFileCreateInfo.MemoryCachingMode = PXMemoryCachingModeSequential;
+        pxFileCreateInfo.FlagList = PXFileIOInfoFileVirtual;
+        pxFileCreateInfo.FileSizeRequest = pxD8TextureTableEntry->RawDataSize;
+
+        PXFileCreate(&pxD8TextureTableEntry->TextureFile, &pxFileCreateInfo);
+
+
+        PXFile* pxFileOUT = pxD8TextureTableEntry->TextureFile;
+
+        PXFileWriteB(pxFileOUT, "DDS ", 4);
+
+        PXDDSHeader pxDDSHeader;
+        PXClear(PXDDSHeader, &pxDDSHeader);
+        pxDDSHeader.Size = 124; // sizeof header
+        pxDDSHeader.Flags = DDSD_CAPS | DDSD_HEIGHT | DDSD_WIDTH | DDSD_PIXELFORMAT;
+        pxDDSHeader.Height = pxD8TextureTableEntry->TextureHeight;
+        pxDDSHeader.Width = pxD8TextureTableEntry->TextureWidth;
+        pxDDSHeader.MipMapCount = (pxD8TextureTableEntry->MinmapCount > 1) ? pxD8TextureTableEntry->MinmapCount : 0;
+
+        int bpp = 0;
+
+        if(pxD8TextureTableEntry->PixelFormat[0] != 0) {
+            // compressed (e.g. DXT1/3/5)
+            pxDDSHeader.Flags |= DDSD_LINEARSIZE;
+            pxDDSHeader.PitchOrLinearSize = pxD8TextureTableEntry->RawDataSize; // total size of top level (or whole chain if you prefer)
+
+            pxDDSHeader.PixelFormatSize = 32;
+            pxDDSHeader.PixelFormatFlags = DDPF_FOURCC;
+            pxDDSHeader.PixelFormatID[0] = pxD8TextureTableEntry->PixelFormat[0];
+            pxDDSHeader.PixelFormatID[1] = pxD8TextureTableEntry->PixelFormat[1];
+            pxDDSHeader.PixelFormatID[2] = pxD8TextureTableEntry->PixelFormat[2];
+            pxDDSHeader.PixelFormatID[3] = pxD8TextureTableEntry->PixelFormat[3];
+
+            // caps
+            pxDDSHeader.Caps1 = DDSCAPS_TEXTURE;
+            if(pxD8TextureTableEntry->MinmapCount > 1) {
+                pxDDSHeader.Caps1 |= DDSCAPS_COMPLEX | DDSCAPS_MIPMAP;
+                pxDDSHeader.Flags |= DDSD_MIPMAPCOUNT;
+            }
+        }
+        else {
+            // uncompressed (e.g. RGBA8)
+            pxDDSHeader.Flags |= DDSD_PITCH;
+            pxDDSHeader.PitchOrLinearSize = (pxD8TextureTableEntry->TextureWidth * bpp + 7) / 8;
+
+            pxDDSHeader.PixelFormatSize = 32;
+            pxDDSHeader.PixelFormatFlags = DDPF_RGB | DDPF_ALPHAPIXELS;
+            pxDDSHeader.RGBBitCount = bpp;
+
+            // assume 32-bit RGBA: 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000
+            if(bpp == 32) {
+                pxDDSHeader.RBitMask = 0x00FF0000;
+                pxDDSHeader.GBitMask = 0x0000FF00;
+                pxDDSHeader.BBitMask = 0x000000FF;
+                pxDDSHeader.ABitMask = 0xFF000000;
+            }
+
+            pxDDSHeader.Caps1 = DDSCAPS_TEXTURE;
+            if(pxD8TextureTableEntry->MinmapCount > 1) {
+                pxDDSHeader.Caps1 |= DDSCAPS_COMPLEX | DDSCAPS_MIPMAP;
+                pxDDSHeader.Flags |= DDSD_MIPMAPCOUNT;
+            }
+        }
+
+        const PXSize readAbount = PXFileBinding
+        (
+            pxFileOUT,
+            &pxDDSHeader,
+            PXDDSHeaderHeaderBindingAA,
+            PXDDSHeaderHeaderBindingSizeAA,
+            PXFileBindingWrite
+        );
+
+        PXAssert(124 == readAbount, "Cant");
+
+        PXFileRead(pxFile, pxFileOUT, pxD8TextureTableEntry->RawDataSize);
+
+
+        PXText pxText;
+        char fileName[64];
+        PXTextFromAdressA(&pxText, fileName, 0, sizeof(fileName));
+        PXTextPrint(&pxText, "J/Q/Unnamed_UUPx%i.dds", i);
+
+        PXFileStoreOnDiskA(pxFileOUT, fileName);
+        PXFileClose(pxFileOUT);
+
+        //PXFileCursorAdvance(pxFile, &pxD8TextureTableEntry->RawDataSize);
+    }
+
+
     return PXResultOK;
 }
 
--- Code/PXUltimaC/PX/Media/D8X/PXD8X.h
+++ Code/PXUltimaC/PX/Media/D8X/PXD8X.h
@@ -8,7 +8,7 @@
 
 
 
-typedef struct PXD8XDatEntry_
+typedef struct PXD8XArchiveEntry_
 {
     PXI32U Hash;
     PXI32U Offset;
@@ -18,36 +18,142 @@ typedef struct PXD8XDatEntry_
 
     PXFile* FileData;
 }
-PXD8XDatEntry;
+PXD8XArchiveEntry;
 
 typedef struct PXD8XDat_
 {
     PXI32U TableEntryAmount;
     PXI32U TableOffset;
 
-    PXD8XDatEntry* PXD8XDatEntry;
+    PXD8XArchiveEntry* PXD8XDatEntry;
 }
 PXD8XDat;
 
 
 
+typedef struct PXD8TextureTableEntry_
+{
+    PXI32U RawDataSize;
+    char PixelFormat[4];
+    PXI32U TextureWidth;
+    PXI32U TextureHeight;
+    PXI32U MinmapCount;
+    PXI32U UnknownB;
+    PXI32U UnknownC;
+    PXI32U UnknownD;
+    PXI32U UnknownE;
+    PXI32U UnknownF;
+    PXI32U UnknownG;
+    PXI32U UnknownH;
+
+    PXFile* TextureFile;
+}
+PXD8TextureTableEntry;
+
+
+typedef struct PXD8TextureSet_
+{
+    PXI32U TextureSetDataSize;
+    PXI32U TextureTotalSize;
+    PXI32U TextureEntryAmount;
+}
+PXD8TextureSet;
+
+typedef struct PXD8TextureTable_
+{ 
+    PXI32U TextureAmount; // Total amount of textures in this file
+    PXI32U SetAmount; // Textures are grouped in sets
+    PXI32U TotalSizeRequested; // All data?
+
+    PXD8TextureSet* SetList;
+
+   // char Unkown[12]; // ????
+
+    PXD8TextureTableEntry* EntryList;
+}
+PXD8TextureTable;
+
+
+
+
+#define PXD8GeometryVertexTypeInvalid 0
+#define PXD8GeometryVertexTypeVerticesSprites 1
+#define PXD8GeometryVertexTypeNormal 2
+
+typedef struct PXD8GeometryHeader_
+{
+    PXI32U VertexType;
+    PXI32U FaceType;
+    PXI32U MeshAmount;
+}
+PXD8GeometryHeader;
+
+
+typedef struct PXD8GeometryVertexFVF_
+{
+    PXI16U Flag;
+    PXI16U Offset;
+    PXI16U Format;
+    PXI8U VertexType;
+    PXI8U UVChannelIndex;
+}
+PXD8GeometryVertexFVF;
+
+typedef struct PXD8GeometryVertexBlock_
+{
+    PXD8GeometryVertexFVF vertices;
+    PXI32U definition_count;
+    PXD8GeometryVertexFVF* definitions;
+    PXI32U BytesBetweenVertices; // stride
+    PXI32U NumberOfVertices;
+}
+PXD8GeometryVertexBlock;
+
+typedef struct PXD8GeometryMesh_
+{
+    PXI32U TextureID;
+    PXI32U Position;
+    PXI32U RadiusBoundingSphere;
+    PXI32U DistanceLODMin;
+    PXI32U DistanceLODMax;
+    PXI32U UnknownA;
+    PXI32U UnknownB;
+    PXI32U UnknownC;
+    PXI32U UnknownD;
+    PXD8GeometryVertexBlock VertexBlock;
+    PXI32U face_count;
+    PXI16U* IndexList;
+    PXI32U node_count;
+    PXMatrix4x4F* nodes;
+}
+PXD8GeometryMesh;
+
+typedef struct PXD8Geometry_
+{
+    PXD8GeometryHeader Header;
+    PXD8GeometryMesh* MeshList;
+}
+PXD8Geometry;
+
 
 
 typedef struct PXD8X_
 {
-    int x;
+    int x;   
 }
 PXD8X;
-
-
-
 
 
 
 PXPublic PXResult PXAPI PXD8XLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo);
 
 PXPublic PXResult PXAPI PXD8DatLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo);
+
+// D8G
+PXPrivate void PXAPI PXD8GGeometryVertexBlock(PXD8GeometryVertexBlock PXREF pxD8GeometryVertexBlock, PXFile PXREF pxFile);
+PXPrivate void PXAPI PXD8GGeometryVertexFVFParse(PXD8GeometryVertexFVF PXREF pxD8GeometryVertexFVF, PXFile PXREF pxFile);
 PXPublic PXResult PXAPI PXD8GLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo);
+
 PXPublic PXResult PXAPI PXD8WLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo);
 PXPublic PXResult PXAPI PXD8TLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo);
 PXPublic PXResult PXAPI PXD8MLoadFromFile(PXECSCreateInfo PXREF pxResourceLoadInfo);
