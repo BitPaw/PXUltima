d407d9b84ebe729024adfcf9e954d4840107b380
BitPaw - the path weaver <NozsBitPaw@gmail.com>
2026-09-16 23:36:48 +0200

    Update PXNamePool.h

--- Code/PXUltimaC/PX/Engine/ECS/PXNamePool.h
+++ Code/PXUltimaC/PX/Engine/ECS/PXNamePool.h
@@ -5,15 +5,44 @@
 
 #include <PX/OS/Error/PXResult.h>
 #include <PX/Engine/PXID.h>
+#include <PX/Type/PXText.h>
+
 
 typedef struct PXNamePool_ PXNamePool;
+
+typedef struct PXNamePoolEntry_
+{
+    union
+    {
+        void* Data;
+        PXASCII* A;
+        PXUNICODE* W;
+    };
+
+    PXSize Length;
+}
+PXNamePoolEntry;
+
+typedef struct PXNamePoolEntryRow_
+{
+    PXText Name;
+    PXID ID;
+}
+PXNamePoolEntryRow;
+
 
 PXPublic PXResult PXAPI PXNamePoolInit();
 
 PXPublic PXResult PXAPI PXNamePoolStore(const PXID pxID, const PXText* pxText, PXText* pxTextResult);
 PXPublic PXResult PXAPI PXNamePoolGet(const PXID pxID, PXText PXREF pxText);
 
-PXPublic PXResult PXAPI PXNamePoolStoreW(const PXID pxID, const wchar_t* nameAdress, const PXSize nameSize, wchar_t** stored);
-PXPublic PXResult PXAPI PXNamePoolStoreA(const PXID pxID, const char* nameAdress, const PXSize nameSize);
+PXPublic PXID PXAPI PXNamePoolFindIDViaName(const PXText PXREF pxText);
+
+PXPublic PXSize PXAPI PXNamePoolEntryAmount();
+PXPublic PXResult PXAPI PXNamePoolEntryGet(PXNamePoolEntryRow PXREF pxNamePoolEntryRow, const PXSize index);
+PXPublic PXResult PXAPI PXNamePoolPrintAll();
+
+PXPublic PXResult PXAPI PXNamePoolStoreW(const PXID pxID, const wchar_t* nameAddress, const PXSize nameSize, wchar_t** stored);
+PXPublic PXResult PXAPI PXNamePoolStoreA(const PXID pxID, const char* nameAddress, const PXSize nameSize);
 
 #endif
